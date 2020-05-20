1901bdb7ca787c9f4c367f331472efa4cc900928
Rodrigo <rodizz@hotmail.com>
2020-05-20 13:12:19 +0200

    Library: update

--- projects/family-hierarchy/src/lib/family-hierarchy.component.ts
+++ projects/family-hierarchy/src/lib/family-hierarchy.component.ts
@@ -1,19 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit, Input, ContentChild, TemplateRef } from '@angular/core';
+import { VisNetworkService } from 'ngx-vis';
+import { OPTION_DEFAULT } from './config/default';
+import { IFamilyNode, IFamilyEdge, IFamilyData } from './models/interfaces';
 
 @Component({
-  selector: 'Fh-FamilyHierarchy',
-  template: `
-    <p>
-      family-hierarchy works!
-    </p>
-  `,
-  styles: []
+  selector: 'FamilyHierarchy',
+  templateUrl: './family-hierarchy.component.html',
+  styleUrls: ['./family-hierarchy.component.scss']
 })
-export class FamilyHierarchyComponent implements OnInit {
 
-  constructor() { }
+export class FamilyHierarchyComponent implements AfterViewInit {
+  @ViewChild('box') el: ElementRef;
+  @ContentChild('nodeTemplate') nodeTemplate: TemplateRef<any>;
+  @ViewChild('contentNode') contentNode: ElementRef;
 
-  ngOnInit(): void {
+  @Input() nodes: IFamilyNode [] = [];
+  @Input() edges: IFamilyEdge [] = [];
+
+  options = OPTION_DEFAULT;
+
+  constructor(private visn: VisNetworkService) {
   }
 
+  ngAfterViewInit(): void {
+    if (this.nodeTemplate) {
+      this.renderNodeTemplate();
+    }
+    const data: IFamilyData = {
+      nodes: this.nodes,
+      edges: this.edges
+    };
+    const a = this.visn.create('1', this.el.nativeElement, data as any, this.options);
+  }
+
+  cleanImage(data: string): string {
+    const reBreak = /\r?\n|\r/g;
+    const reStyles = /\_ngcontent.*?""/g;
+    const regex = /<\!--.*?-->/g;
+
+    return data.replace(reBreak, '').replace(reStyles, '').replace(regex, '');
+  }
+
+  renderNodeTemplate(): void {
+    const nodeTemplate = this.contentNode.nativeElement.innerHTML;
+    const url = 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent(this.cleanImage(nodeTemplate));
+    this.nodes.forEach(node => {
+      node.image = url;
+    });
+  }
 }
--- projects/family-hierarchy/src/lib/family-hierarchy.module.ts
+++ projects/family-hierarchy/src/lib/family-hierarchy.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { FamilyHierarchyComponent } from './family-hierarchy.component';
-
+import { VisModule } from 'ngx-vis';
+import { CommonModule } from '@angular/common';
 
 
 @NgModule({
   declarations: [FamilyHierarchyComponent],
   imports: [
+    CommonModule,
+    VisModule,
   ],
   exports: [FamilyHierarchyComponent]
 })
