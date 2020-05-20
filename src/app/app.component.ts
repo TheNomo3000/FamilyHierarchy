5c373dceab14a0bfeca7e418f272af0b6614bb5d
Rodrigo <rodizz@hotmail.com>
2020-05-20 13:10:45 +0200

    Demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { IFamilyNode, IFamilyEdge } from 'projects/family-hierarchy/src/lib/models/interfaces';
+import { EDGES_EXAMPLES, NODES_EXAMPLES } from './data';
 
 @Component({
-  selector: 'Fh-root',
+  selector: 'root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  edges: IFamilyEdge [] = EDGES_EXAMPLES;
+  nodes: IFamilyNode [] = NODES_EXAMPLES;
   title = 'FamilyHierarchyDemo';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FamilyHierarchyModule } from 'projects/family-hierarchy/src/public-api';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FamilyHierarchyModule
   ],
   providers: [],
   bootstrap: [AppComponent]
